9eea853dbf16da814aa0745e6db9a26588afbc28
JerickTwO <jerickjjtm774@gmail.com>
2024-04-28 19:03:17 -0500

    changes the color in the button submit

--- index.php
+++ index.php
@@ -33,9 +33,12 @@ if ($conn->connect_error) {
             <!-- Select para motorizados (solo selección única) -->
             <select class="selectpicker" name="motorizado">
                 <?php
+
                 // Consulta para obtener los nombres de los motorizados
                 $sql_motorizados = "SELECT id_motorizado, nombre FROM motorizado";
                 $result_motorizados = $conn->query($sql_motorizados);
+
+                
 
                 // Mostrar opciones de motorizados
                 if ($result_motorizados->num_rows > 0) {
@@ -66,16 +69,14 @@ if ($conn->connect_error) {
                 }
                 ?>
             </select>
-                <button type="submit">Pedir</button>
+                <button class="btn btn-secondary" type="submit">Pedir</button>
         </form>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/js/bootstrap-select.min.js" integrity="sha512-FHZVRMUW9FsXobt+ONiix6Z0tIkxvQfxtCSirkKc5Sb4TKHmqq1dZa8DphF0XqKb3ldLu/wgMa8mT6uXiLlRlw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
 </body>
-
 </html>
 
 <?php
